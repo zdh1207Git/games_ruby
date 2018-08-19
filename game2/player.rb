d669e33f5dff572435c01c6affce2a05cdb560aa
zdh1207git <gotoseeyou@qq.com>
2018-08-19 11:03:00 +0800

    first commit

---
+++ game2/player.rb
@@ -0,0 +1,21 @@
+class Player
+
+  def initialize(window)
+
+    @x = 200
+
+    @y = 200
+
+    @angle = 0
+
+    @image = Gosu::Image.new('images/cannon1.png')
+
+  end
+
+  def draw
+
+    @image.draw_rot(@x, @y, 1, @angle)
+
+  end
+
+end
